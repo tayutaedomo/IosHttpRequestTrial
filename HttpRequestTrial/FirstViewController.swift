9537f6738fb978aa9b1a6b6ac92b7c3db03b00e4
tayutaedomo <tayutaedomo@gmail.com>
2019-08-24 09:49:37 +0900

    Add project base files

---
+++ HttpRequestTrial/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HttpRequestTrial
+//
+//  Created by tayutaedomo on 2019/08/24.
+//  Copyright © 2019 tayutaedomo.net. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HttpRequestTrial/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HttpRequestTrial
+//
+//  Created by tayutaedomo on 2019/08/24.
+//  Copyright © 2019 tayutaedomo.net. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
