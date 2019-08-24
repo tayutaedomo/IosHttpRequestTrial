6ec630854f4ad6f71ab06e1bbf92f7f9e259f2a1
tayutaedomo <tayutaedomo@gmail.com>
2019-08-24 15:06:02 +0900

    Add URLSession handling

--- HttpRequestTrial/WebAPI.swift
+++ HttpRequestTrial/WebAPI.swift
@@ -18,22 +18,62 @@ typealias Input = Request
 
 enum WebAPI {
 
-    static func call(with input: Input, _ block: @escaping (Output) -> Void) {
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
-            let response: Response = (
-                statusCode: .ok,
-                headers: [:],
-                payload: "this is a response text".data(using: .utf8)!
-            )
-
-            block(.hasResponse(response))
-        }
-    }
-
     static func call(with input: Input) {
         self.call(with: input) { _ in
             // TODO
         }
+    }
+
+    static func call(with input: Input, _ block: @escaping (Output) -> Void) {
+//        Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+//            let response: Response = (
+//                statusCode: .ok,
+//                headers: [:],
+//                payload: "this is a response text".data(using: .utf8)!
+//            )
+//
+//            block(.hasResponse(response))
+//        }
+
+        let urlRequest = self.createUrlRequest(by: input)
+
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, urlResponse, error) in
+
+            let output = self.createOutput(
+                data: data,
+                urlResponse: urlResponse as? HTTPURLResponse,
+                error: error)
+
+            block(output)
+        }
+        task.resume()
+    }
+
+    static private func createUrlRequest(by input: Input) -> URLRequest {
+        var request = URLRequest(url: input.url)
+
+        request.httpMethod = input.methodAndPayload.method
+        request.httpBody = input.methodAndPayload.body
+        request.allHTTPHeaderFields = input.headers
+
+        return request
+    }
+
+    static private func createOutput(data: Data?, urlResponse: HTTPURLResponse?, error: Error?) -> Output {
+        guard let data = data, let response = urlResponse else {
+            return .noResponse(.noDataOrNoResponse(debugInfo: error.debugDescription))
+        }
+
+        var headers: [String: String] = [:]
+        for (key, value) in response.allHeaderFields.enumerated() {
+            headers[key.description] = String(describing: value)
+        }
+
+        return .hasResponse((
+            statusCode: .from(code: response.statusCode),
+            headers: headers,
+            payload: data
+        ))
     }
 }
 
